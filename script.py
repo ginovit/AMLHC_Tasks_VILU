41053a36e1799d9df3bfb40ab65ce13df5cc9365
Gino <gino@372fa50e-f406-4f22-b09f-e8a6419172d6.fritz.box>
2025-03-09 11:11:34 +0100

    my notebook intro.ipynb with calc mean and iris data set

--- script.py
+++ script.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from sklearn import datasets
 
-# Funktion zur Berechnung der Mittelwerte
+# func Mean
 def calcMeans(dataframe):
     return dataframe.apply(lambda x: x.mean())
 
-# Test mit kleinem DataFrame
+# DataFrame
 test_df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
 print("Test DataFrame Mean:")
 print(calcMeans(test_df))
 print(test_df.describe())
 
-# Iris-Datensatz laden und Funktion anwenden
+# Load and print
 iris_X, iris_y = datasets.load_iris(return_X_y=True, as_frame=True)
 mean_values = calcMeans(iris_X)
 print("\nIris DataFrame Mean:")
